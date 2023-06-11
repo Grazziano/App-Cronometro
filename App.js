b01a3771e2be64ce17af9cb218e18ead07b817bc
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-06-11 10:07:30 -0300

    Starting and stopping stopwatch

--- App.js
+++ App.js
@@ -1,24 +1,86 @@
+import { useState } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+let timer = null;
+let ss = 0;
+let mm = 0;
+let hh = 0;
+
 export default function App() {
+  const [number, setNumber] = useState(0);
+  const [button, setButton] = useState('START');
+  const [last, setLast] = useState(null);
+
+  function start() {
+    if (timer !== null) {
+      // Aqui vai parar o timer
+      clearInterval(timer);
+      timer = null;
+
+      setButton('START');
+    } else {
+      // Comecar a girar o timer...
+      timer = setInterval(() => {
+        ss++;
+        if (ss === 60) {
+          ss = 0;
+          mm++;
+        }
+
+        if (mm === 60) {
+          mm = 0;
+          hh++;
+        }
+
+        let format =
+          (hh < 10 ? '0' + hh : hh) +
+          ':' +
+          (mm < 10 ? '0' + mm : mm) +
+          ':' +
+          (ss < 10 ? '0' + ss : ss);
+
+        setNumber(format);
+      }, 1000);
+
+      setButton('STOP');
+    }
+  }
+
+  function clear() {
+    if (timer !== null) {
+      //Parar o timer
+      clearInterval(timer);
+      timer = null;
+    }
+
+    setLast(number);
+    setNumber(0);
+    ss = 0;
+    mm = 0;
+    hh = 0;
+    setButton('START');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require('./src/crono.png')} />
 
-      <Text style={styles.timer}>00:00:00</Text>
+      <Text style={styles.timer}>{number}</Text>
 
       <View style={styles.btnArea}>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.btnText}>VAI</Text>
+        <TouchableOpacity style={styles.btn} onPress={start}>
+          <Text style={styles.btnText}>{button}</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btn}>
+        <TouchableOpacity style={styles.btn} onPress={clear}>
           <Text style={styles.btnText}>LIMPAR</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.areaUltima}>
-        <Text style={styles.textoCorrida}>Último tempo 00:03:50</Text>
+        <Text style={styles.textoCorrida}>
+          {last ? 'Último tempo ' + last : ''}
+        </Text>
       </View>
     </View>
   );
